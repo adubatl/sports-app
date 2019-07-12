fc7229a17cfa5664f027a45e888ad93d3cf5a38a
Lucas <lkm027@gmail.com>
2019-07-12 19:38:22 -0400

    Working commit. Setting up navigationContainer

--- App.tsx
+++ App.tsx
@@ -1,18 +1,42 @@
 import React from "react";
 
-import styled from "styled-components/native";
+import { StyleSheet } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+// Pages
 import LoginPage from "./Pages/Login";
+import SignUpPage from "./Pages/SignUp";
 
-const AppContainer = styled.View`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+const styles = StyleSheet.create({
+    appContainer : {
+        display        : "flex",
+        justifyContent : "center",
+        alignItems     : "center"
+    },
+});
 
-export default function App() {
-  return (
-    <AppContainer>
-      <LoginPage />
-    </AppContainer>
-  );
+const AppNavigator = createStackNavigator(
+    {
+        Login  : LoginPage,
+        SignUp : SignUpPage,
+    },
+    {
+        initialRouteName : 'Login',
+        defaultNavigationOptions : {
+            headerStyle : {
+                backgroundColor : '#D88'
+            },
+        }
+    }
+);
+
+const AppContainer = createAppContainer( AppNavigator );
+
+// Better to have more control over the component at the root of the app
+// than you would get from exporting the result of `createAppContainer`.
+export default class App extends React.Component {
+    render() {
+
+        return <AppContainer />
+        }
 }
