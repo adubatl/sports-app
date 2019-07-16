aeede21826e18fdd9456510b44e82070e3ab60ec
Lucas <lkm027@gmail.com>
2019-07-16 19:29:11 -0400

    Created base home directory

--- Pages/HomeDashboard.tsx
+++ Pages/HomeDashboard.tsx
@@ -1,46 +1,51 @@
 import React from "react";
 
-import styled        from "styled-components/native";
-import { View }      from "react-native";
-import { ListItem }  from "react-native-elements";
+import styled                     from "styled-components/native";
+import { View, Text, StyleSheet } from "react-native";
+import { ListItem }               from "react-native-elements";
 
-const LoginView = styled.View`
-  display         : flex;
-  flex-direction  : column;
-  align-items     : center;
-  justify-content : center;
-  width           : 100%;
-  height          : 100%;
-`;
+const styles = StyleSheet.create({
+  listContainer : {
+    borderRadius: 4,
+    borderWidth: 5,
+    borderColor: "#000",
+  },
 
-const LoginPanel = styled.View`
-  display          : flex;
-  flex-direction   : column;
-  padding          : 30px;
-  justify-content  : center;
-  align-items      : center;
-  border-radius    : 8px;
-  background-color : lightblue;
-  width            : 50%;
-  height           : 30%;
-`;
+  title : {
+    backgroundColor : "#AAA",
+  }
+});
 
-const LoginField = styled.View`
-  display : flex;
-  width   : 75%;
-`;
+class ListSection extends React.Component {
+  constructor( props ) {
+    super( props );
+  }
 
-const LoginHeader = styled.Text`
-  font-size     : 18;
-  text-align    : center;
-  margin-bottom : 30px;
-`;
-
-const LoginLogo = styled.Text`
-  width  : 100%;
-  height : 20px;
-  border : 1px solid purple;
-`;
+  render() {
+    return(
+      <View>
+        <View>
+          <Text style = { styles.title }>{ this.props.title }</Text>
+        </View>
+        { this.props.items.length > 0 ?
+          (
+              this.props.items.map( ( item, index ) => (
+                <ListItem
+                  key   = { index }
+                  title = { item }
+                />
+              ))
+          ) :
+          (
+            <ListItem
+              title = { 'No ' + this.props.title }
+            />
+          )
+        }
+      </View>
+    );
+  }
+}
 
 interface IProps {}
 
@@ -50,26 +55,21 @@ export default class LoginPage extends React.PureComponent<IProps, IState> {
   constructor(props) {
     super(props);
     this.state = {
-      teams : [ "Team 1", "Team 2", "Team 3" ]
+      teams  : [ "Team 1", "Team 2", "Team 3" ],
+      events : []
     };
-
-    this.state.teams.forEach( (teamName, index ) => {
-      console.log( teamName + " " + index );
-    });
   }
 
   render() {
     return (
-      <View>
-        {
-          this.state.teams.map( ( teamName, index ) => (
-            <ListItem
-              key   = { index }
-              title = { teamName }
-            />
-          ))
-        }
-      </View>
+        <View>
+          <View>
+            <ListSection items={this.state.teams} title="Teams">
+            </ListSection>
+            <ListSection items={this.state.events} title="Events">
+            </ListSection>
+          </View>
+        </View>
     );
   }
 }
